82dcc3fdab03bb7f6fbae13970973b40bb87894b
corinabiro <corina.biro@gmail.com>
2022-10-22 15:24:58 +0300

    Add navigation and styles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import "./index.css";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(
+  <BrowserRouter>
+    <App tab="home" />
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
