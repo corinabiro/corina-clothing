6674b62cfb5329096f5c6d89d74d2ef82a363773
corinabiro <corina.biro@gmail.com>
2022-11-07 22:04:16 +0200

    Adding redirect for Netlify

--- src/components/sign-up-form/sign-up-form-component.jsx
+++ src/components/sign-up-form/sign-up-form-component.jsx
@@ -53,7 +53,7 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
+    <SignUpContainer>
       <h2>Don't have an account?</h2>
       <span>Sign Up with your email and password</span>
       <form onSubmit={handleSubmit}>
@@ -91,7 +91,7 @@ const SignUpForm = () => {
         />
         <Button type="submit">Sign Up</Button>
       </form>
-    </div>
+    </SignUpContainer>
   );
 };
 
