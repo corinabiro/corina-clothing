3f3d1ddc5469b327a6e58965d2c48d0d9c8694b7
corinabiro <corina.biro@gmail.com>
2022-11-15 14:17:46 +0200

    add redux persist

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,11 +12,13 @@ import reportWebVitals from "./reportWebVitals";
 const container = document.getElementById("root");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App tab="home" />
-    </BrowserRouter>
-  </Provider>
+  <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App tab="home" />
+      </BrowserRouter>
+    </Provider>
+  </PersistGate>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,33 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistsedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+export const store = createStore(
+  persistsedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
